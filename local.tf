7d0fe956bcf9e20f2a8af46262ad50cb963b7d8d
bkarakas <bkarakasu@gmail.com>
2020-01-26 15:35:51 -0600

    comman line chnaged

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "login" {
   provisioner "local-exec" {
-    command = "aws eks --region us-west-2 update-kubeconfig --name dev-cluster"
+    command = "aws eks --region echo $region update-kubeconfig --name echo $cluster_name"
     
   }
 }
