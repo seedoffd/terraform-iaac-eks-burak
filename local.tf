419564d18ed37904cd55e40814bd83840b3ca166
bkarakas <bkarakasu@gmail.com>
2020-01-28 20:33:24 -0600

    mv kubectl to /bin

--- local.tf
+++ local.tf
@@ -4,10 +4,13 @@ resource "null_resource" "login" {
     command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_name}"
   }
     provisioner "local-exec" {
-    command = "sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
+    command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/${var.cluster_version}/bin/linux/amd64/kubectl"
   }
     provisioner "local-exec" {
     command = "sudo chmod +x kubectl"
+  }
+    provisioner "local-exec" {
+    command = "mv kubectl /bin/"
   }
     provisioner "local-exec" {
     command = "curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator"
