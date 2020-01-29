2e1d6e156591a3d873c4b08d1d03f2bbd4e7cbd1
bkarakas <bkarakasu@gmail.com>
2020-01-29 14:53:37 -0600

    multiple outputs added

--- configurations/dev/us-west-2/dev.tfvars
+++ configurations/dev/us-west-2/dev.tfvars
@@ -16,3 +16,4 @@ asg_min_size = "3"
 asg_desired_capacity = "3"
 cluster_version = "1.14"
 
+
--- variable.tf
+++ variable.tf
@@ -15,3 +15,4 @@ variable "s3_tfstate_file" {}
 variable "s3_bucket" {}
 variable "region" {}
 variable "asg_desired_capacity" {}
+
